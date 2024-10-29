4b1c6b2aff64f01bb0191b06e6d49194257b6a60
marvinmilien <marvinmilien@gmail.com>
2024-10-29 14:52:39 -0400

    user authentication

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,10 +3,19 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    model = CustomUser
-    fields = UserCreationForm.Meta.fields + ("age",)
+    class Meta:
+        model = CustomUser
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
